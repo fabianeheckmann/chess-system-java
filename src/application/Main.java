d0655b849b8737ecfc941cbc9c4122feaa1ebf60
fabianeheckmann <fabiane.heckmanns@gmail.com>
2024-02-08 13:50:02 -0300

    feat: starting to implement Board and Piece

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,11 @@
 package application;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import boardgame.Board;
+
 public class Main {
     public static void main(String[] args) {
 
+        Board board = new Board(8, 8);
 
     }
 }
