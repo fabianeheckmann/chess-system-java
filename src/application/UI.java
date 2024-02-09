4de692f7cb7bc91a1724a4514df0e84e13a14fde
fabianeheckmann <fabiane.heckmanns@gmail.com>
2024-02-09 10:21:37 -0300

    feat: BoardException and defensive programming

--- src/application/UI.java
+++ src/application/UI.java
@@ -5,7 +5,7 @@ import chess.ChessPiece;
 public class UI {
 
     public static void printBoard(ChessPiece[][] pieces) {
-        for (int i = 0; i< pieces.length; i++) {
+        for (int i = 0; i < pieces.length; i++) {
             System.out.print(8 - i + " ");
             for (int j = 0; j < pieces.length; j++) {
                 printPiece(pieces[i][j]);
@@ -17,10 +17,10 @@ public class UI {
 
     private static void printPiece(ChessPiece piece) {
         if (piece == null) {
-            System.out.print("- ");
+            System.out.print("-");
         } else {
             System.out.print(piece);
         }
-        System.out.print("");
+        System.out.print(" ");
     }
 }
